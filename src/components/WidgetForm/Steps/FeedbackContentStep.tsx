55571e7be3807fd701d7eb0d6c50ac3b0bba372e
unknown <54408431+Samuel-Denis@users.noreply.github.com>
2022-05-09 21:59:22 -0300

    finalizado

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,24 +1,42 @@
 import { ArrowLeft } from "phosphor-react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
+import { ScreenshotButto } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
-    onFeedbackRestartRequested : () => void
+    onFeedbackSent: () => void
+    onFeedbackRestartRequested: () => void;
 }
 
-export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps) {
+export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, onFeedbackSent }: FeedbackContentStepProps) {
 
-    const feedbackTypeInfo =  feedbackTypes[feedbackType]
+
+    const [screenshot, setScreenshot] = useState<string | null>(null)
+    const [ comment, setComment] = useState('')
+
+    const feedbackTypeInfo = feedbackTypes[feedbackType]
+
+    function handleSubmitFeedback(event: FormEvent){
+        event.preventDefault();
+
+        console.log(
+            screenshot,
+            comment
+        )
+
+        onFeedbackSent();
+    }
 
     return (
         <>
             <header>
-                <button 
-                type="button" 
-                onClick= {onFeedbackRestartRequested}
-                className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100">
-                    <ArrowLeft weight="bold" className="w-4 h-4"/>
+                <button
+                    type="button"
+                    onClick={onFeedbackRestartRequested}
+                    className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100">
+                    <ArrowLeft weight="bold" className="w-4 h-4" />
                 </button>
                 <span className="text-xl leading-6 flex items-center gap-2">
                     <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
@@ -26,21 +44,29 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
                 <CloseButton />
             </header>
 
-            <form className="my-4 w-full">
-                <textarea 
+            <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
+                <textarea
                     className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin resize-none"
-                    placeholder="Conte com detalhes o que está acontecendo...."/>
+                    placeholder="Conte com detalhes o que está acontecendo...."
+                    onChange={event => setComment(event.target.value)}
+                    />
+
+
+                <footer className="flex gap-2 mt-2">
+
+                    <ScreenshotButto 
+                        screenshot={screenshot}
+                        onScreenshotTook={setScreenshot}/>
+
+                    <button
+                        disabled={comment.length === 0}
+                        type="submit"
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 disabled:opacity-50 disabled:hover:bg-brand-500"
+                    >
+                        Enviar feedback
+                    </button>
+                </footer>
             </form>
-
-
-            <footer className="flex gap-2 mt-2">
-                <button
-                    type="submit"
-                    className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
-                >
-                Enviar feedback
-                </button>
-            </footer>
         </>
     )
 }
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -6,6 +6,7 @@ import thoughtImageUrl from '../../assets/thought.svg';
 
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 
 
@@ -40,16 +41,20 @@ export type FeedbackType = keyof typeof feedbackTypes
 export function WidgetForm() {
 
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+    const [ feedbackSent, setFeedbanckSent] = useState(false);
 
     function handleRestartFeedback(){
+        setFeedbanckSent(false);
         setFeedbackType(null);
     }
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
-
-            {
+            { feedbackSent ? (
+                <FeedbackSuccessStep  onFeedbackSent={handleRestartFeedback}/>
+            ): <>
+                        {
                 !feedbackType ?
                     (
                         <FeedbackTypeStep onFeedbackTypeChange={setFeedbackType}/>
@@ -57,10 +62,12 @@ export function WidgetForm() {
                     : (
                         <FeedbackContentStep 
                             feedbackType={feedbackType}
+                            onFeedbackSent={() => setFeedbanckSent(true)}
                             onFeedbackRestartRequested={handleRestartFeedback}
                             />
                     )
-            }
+            }</>
+        }
 
             <footer className="text-xl text-neutral-400">
                 Feito com ❤ pela <a className="underline underline-offset-2" href="https://www.rocketseat.com.br/">Rocketseat</a>
